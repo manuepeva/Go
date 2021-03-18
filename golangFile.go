25de30870613946e9e871c2b17ec119cb0762857
ManuelP <manuepeva@gmail.com>
2021-03-17 20:43:34 -0500

    strutil file

---
+++ golangFile.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+	name := "Ronny"
+	fmt.Println("This is a test of Go")
+	age := 18
+	fmt.Printf("%T\n", age)
+	fmt.Println((name))
+	fmt.Println((age))
+}
